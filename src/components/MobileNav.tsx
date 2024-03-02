421f578d5253bcc1aa1064e943d302fcd4d329b0
saih3 <sailyonh@gmail.com>
2024-03-02 11:30:14 -0500

    Adding all files
    Finished responsive landing page

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "@radix-ui/react-separator";
+import { Button } from "./ui/button";
+
+export default function MobileNav() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+        <SheetContent className="space-y-3">
+          <SheetTitle>
+            <span>Welcome to DeliDash.co</span>
+          </SheetTitle>
+          <Separator />
+          <SheetDescription className="flex">
+            <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+          </SheetDescription>
+        </SheetContent>
+      </SheetTrigger>
+    </Sheet>
+  );
+}
