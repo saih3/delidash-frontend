77989e2a73ceb393610ab4de7f06c55a1242d998
saih3 <sailyonh@gmail.com>
2024-03-07 22:56:35 -0500

    User profile form done and update request working

---
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,137 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import LoadingButton from "@/components/LoadingButton";
+
+const formSchema = z.object({
+  email: z.string().optional(),
+  name: z.string().min(1, "Name is required"),
+  addressLine1: z.string().min(1, "AddressLine1 is required"),
+  country: z.string().min(1, "Country is required"),
+  city: z.string().min(1, "City is required"),
+});
+
+type UserFormData = z.infer<typeof formSchema>;
+
+type Props = {
+  onSave: (userProfileDate: UserFormData) => void;
+  isLoading: boolean;
+};
+
+export default function UserProfileForm({ isLoading, onSave }: Props) {
+  // initializing react hook library
+  const form = useForm<UserFormData>({
+    resolver: zodResolver(formSchema),
+  });
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSave)}
+        className="space-y-4 bg-gray-50 rounded-lg md:p-10"
+      >
+        <div>
+          <h2 className="text-2xl font-bold"> User Profile Form</h2>
+          <FormDescription>
+            View and change your profile information here
+          </FormDescription>
+        </div>
+        {/* Email form field */}
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} disabled className="bg-white" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        {/* Name  */}
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} className="bg-white" />
+              </FormControl>
+              {/* FormMessage shows error when no input is given  */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* Address fields */}
+        <div className="flex flex-col md:flex-row gap-4">
+          {/* addressLine1  */}
+          <FormField
+            control={form.control}
+            name="addressLine1"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Address Line 1</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* City  */}
+          <FormField
+            control={form.control}
+            name="city"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>City</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Country  */}
+          <FormField
+            control={form.control}
+            name="country"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Country</FormLabel>
+                <FormControl>
+                  <Input {...field} className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        {isLoading ? (
+          <LoadingButton />
+        ) : (
+          <Button type="submit" className="bg-orange-500">
+            Submit
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+}
