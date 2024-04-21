fb0bcf156190275fb12df9db4e330272bd8708d4
saih3 <sailyonh@gmail.com>
2024-04-21 13:52:52 -0400

    Create restaurant form and API
    7h:46m

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="bg-orange-500 py-10">
+    <div className="bg-orange-500 py-5">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <span className="text-3xl text-white font-bold tracking-tight">
           delidashapp.com
---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,9 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
+import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+
+export default function ManageRestaurantPage() {
+  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  return (
+    <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+  );
+}
